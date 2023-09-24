7a09db2d6361872702913f65c82522a710a3b6e6
N8BWert <n8.wert.b@gmail.com>
2023-09-24 11:36:11 -0400

    write radio test

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,30 @@
 #![no_main]
 #![feature(type_alias_impl_trait)]
 
+extern crate alloc;
+
+use embedded_alloc::Heap;
+
+#[global_allocator]
+static HEAP: Heap = Heap::empty();
+
 use teensy4_panic as _;
 
 #[rtic::app(device = teensy4_bsp, peripherals = true)]
 mod app {
+    use super::*;
+
+    use core::mem::MaybeUninit;
+
     use teensy4_bsp as bsp;
     use bsp::board;
 
     use bsp::hal as hal;
 
     use bsp::ral as ral;
+
+    const HEAP_SIZE: usize = 1024;
+    static mut HEAP_MEM: [MaybeUninit<u8>; HEAP_SIZE] = [MaybeUninit::uninit(); HEAP_SIZE];
 
     #[local]
     struct Local {}
@@ -21,6 +35,8 @@ mod app {
 
     #[init]
     fn init(ctx: init::Context) -> (Shared, Local) {
+        unsafe { HEAP.init(HEAP_MEM.as_ptr() as usize, HEAP_SIZE) }
+
         (
             Shared {},
             Local {},
