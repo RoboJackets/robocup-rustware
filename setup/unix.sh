4904746209a6139ef6de1e478c04db3a806fb202
Isaiah Gamble <77396670+tsar-boomba@users.noreply.github.com>
2023-10-04 02:35:40 +0000

    Improve docs for setup to include mac directions (#2)
    * Improve docs for setup to include mac directions
    
    * Add install cargo-binutils to setup script
    
    * Remove 'drivers/rgb_led' from Cargo.toml so rust_analyzer will work
    
    * fix OS variable for mac setup & style fixes
    
    * add teensy-loader-cli ubuntu dependencies + rustup target + llvm-tools-preview + nightly
    
    * update documentation to be more specific about what the RGB Led driver is
    
    ---------
    
    Co-authored-by: N8BWert <n8.wert.b@gmail.com>

---
+++ setup/unix.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+currentDir="$(cd "$(dirname "${BASH_SOURCE[0]}")" > /dev/null 2>&1 && pwd)"
+
+if ! command -v 'cargo'; then
+	echo 'You need to install Rust onto your computer before starting this tutorial. https://www.rust-lang.org/tools/install'
+	exit 1
+fi
+
+echo "Installing cargo-binutils for rust-objcopy"
+if cargo --list | grep -q 'binstall'; then
+	echo "Using cargo binstall"
+	cargo binstall cargo-binutils
+else
+	echo "Using cargo install"
+	cargo install cargo-binutils
+fi
+
+echo "Adding Teensy Target and llvm-tools-preview"
+rustup target add thumbv7em-none-eabihf
+rustup component add llvm-tools-preview
+rustup default nightly
+
+# Linux Needs libusb-dev
+if type apt-get > /dev/null 2>&1; then
+	sudo apt-get update && apt-get install -y libusb-dev
+fi
+
+echo "Cloning Teensy Loader CLI GitHub Repository"
+git clone https://github.com/PaulStoffregen/teensy_loader_cli.git "${currentDir}/teensy_loader_cli"
+
+echo "${currentDir}"
+
+echo "Making Teensy Loader CLI"
+make -C "${currentDir}/teensy_loader_cli"
+
+echo "Moving Executable"
+mv "${currentDir}/teensy_loader_cli/teensy_loader_cli" ${currentDir}/../teensy_loader_cli
+
+echo "Cleaning Up"
+rm -rf "${currentDir}/teensy_loader_cli"
