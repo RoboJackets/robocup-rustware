48f2c80e7f10cf4b2602df88e6f6dca8ef6c838d
tsar-boomba <itg.2048@gmail.com>
2023-10-17 19:49:13 -0400

    initial driver impl

---
+++ drivers/icm42605-driver/src/lib.rs
@@ -0,0 +1,140 @@
+#![no_std]
+
+use embedded_hal::blocking::{i2c, delay::DelayMs};
+use registers::{Bank, BankSelect, WHO_AM_I};
+
+pub mod registers;
+
+const WHO_AM_I_EXPECTED: u8 = 0x42;
+const LSB_TO_G: f64 = 4.0 / 32768.0;
+
+pub struct Icm42605<I2C> {
+    i2c: I2C,
+    current_bank: Bank,
+}
+
+impl<
+        I2C: i2c::Write<Error = E> + i2c::Read<Error = E> + i2c::WriteRead<Error = E> + 'static,
+        E
+    > Icm42605<I2C>
+{
+    pub fn new(i2c: I2C, delay: &mut impl DelayMs<u8>) -> Result<Self, E> {
+        let mut this = Self {
+            i2c,
+            current_bank: Default::default(),
+        };
+
+        while this.read(Bank::Bank0, WHO_AM_I)? != WHO_AM_I_EXPECTED {
+            // TODO: log???
+            delay.delay_ms(100);
+        }
+
+        // Reset
+        this.write(
+            registers::DeviceConfig::BANK,
+            registers::DeviceConfig::ADDR,
+            registers::DeviceConfig::SOFT_RESET_CONFIG.bits()
+                | registers::DeviceConfig::SPI_MODE_0_AND_3.bits(),
+        )?;
+
+        delay.delay_ms(100);
+
+        // Check whoami again
+        while this.read(Bank::Bank0, WHO_AM_I)? != WHO_AM_I_EXPECTED {
+            // TODO: log???
+            delay.delay_ms(100);
+        }
+
+        delay.delay_ms(100);
+
+        // Turn on the gyro and accel
+        this.write(
+            registers::PowerManagement::BANK,
+            registers::PowerManagement::ADDR,
+            registers::PowerManagement::GYRO_LOW_NOISE.bits()
+                | registers::PowerManagement::ACCEL_LOW_NOISE.bits(),
+        )?;
+
+        // From 14.36:
+        // Gyroscope needs to be kept ON for a minimum of 45ms. When transitioning from OFF to any of the other modes, do not issue any register writes for 200μs.
+        delay.delay_ms(100);
+
+        // Configure gyro and accelerometer
+        this.write(
+            registers::GyroConfig::BANK,
+            registers::GyroConfig::ADDR,
+            registers::GyroConfig::GYRO_FS_1000.bits()
+                | registers::GyroConfig::GYRO_ODR_1kHz.bits(),
+        )?;
+
+        this.write(
+            registers::AccelConfig::BANK,
+            registers::AccelConfig::ADDR,
+            registers::AccelConfig::ACCEL_FS_16.bits()
+                | registers::AccelConfig::ACCEL_ODR_1kHz.bits(),
+        )?;
+
+        Ok(this)
+    }
+
+    pub fn gyro_z(&mut self) -> Result<f64, E> {
+        let hi = self.read(Bank::Bank0, registers::GYRO_DATA_Z1)?;
+        let lo = self.read(Bank::Bank0, registers::GYRO_DATA_Z0)?;
+
+        // Reinterpret the bits as a signed 16-bit integer
+        let total = u16::from_ne_bytes([hi, lo]);
+
+        // +-1000dps max, for 16-bit signed integers, comes out to 32.8lsb/deg.
+        const LSB_TO_DPS: f64 = 1000.0 / 32768.0;
+        const DEG_TO_RADIANS: f64 = core::f64::consts::PI / 180.0;
+
+        Ok(total as f64 * (DEG_TO_RADIANS * LSB_TO_DPS))
+    }
+
+    pub fn accel_x(&mut self) -> Result<f64, E> {
+        let hi = self.read(Bank::Bank0, registers::ACCEL_DATA_X1)?;
+        let lo = self.read(Bank::Bank0, registers::ACCEL_DATA_X0)?;
+
+        // Reinterpret the bits as a signed 16-bit integer
+        let total = u16::from_ne_bytes([hi, lo]);
+
+        // +-4g max, for 16-bit signed integers, comes out to 8192lsb/g.
+        Ok(total as f64 * LSB_TO_G)
+    }
+
+    pub fn accel_y(&mut self) -> Result<f64, E> {
+        let hi = self.read(Bank::Bank0, registers::ACCEL_DATA_Y1)?;
+        let lo = self.read(Bank::Bank0, registers::ACCEL_DATA_Y0)?;
+
+        // Reinterpret the bits as a signed 16-bit integer
+        let total = u16::from_ne_bytes([hi, lo]);
+
+        // +-4g max, for 16-bit signed integers, comes out to 8192lsb/g.
+        Ok(total as f64 * LSB_TO_G)
+    }
+
+    fn switch_bank(&mut self, new_bank: Bank) -> Result<(), E> {
+        self.i2c.write(BankSelect::ADDR, &[new_bank.value()])?;
+        self.current_bank = new_bank;
+        Ok(())
+    }
+
+    fn read(&mut self, bank: Bank, address: u8) -> Result<u8, E> {
+        if bank != self.current_bank {
+            self.switch_bank(bank)?;
+        }
+
+        let mut buf = [0];
+        self.i2c.read(address, &mut buf)?;
+        Ok(buf[0])
+    }
+
+    fn write(&mut self, bank: Bank, address: u8, value: u8) -> Result<(), E> {
+        if bank != self.current_bank {
+            self.switch_bank(bank)?;
+        }
+
+        self.i2c.write(address, &[value])?;
+        Ok(())
+    }
+}
---
+++ drivers/icm42605-driver/src/registers.rs
@@ -0,0 +1,135 @@
+use bitflags::bitflags;
+
+pub const ACCEL_DATA_X1: u8 = 0x1F;
+pub const ACCEL_DATA_X0: u8 = 0x20;
+pub const ACCEL_DATA_Y1: u8 = 0x21;
+pub const ACCEL_DATA_Y0: u8 = 0x22;
+pub const GYRO_DATA_Z1: u8 = 0x29;
+pub const GYRO_DATA_Z0: u8 = 0x2A;
+pub const WHO_AM_I: u8 = 0x75;
+
+bitflags! {
+    #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+    pub struct BankSelect: u8 {
+        const BANK_0 = 0;
+        const BANK_1 = 1;
+        const BANK_2 = 2;
+        const BANK_3 = 3;
+        const BANK_4 = 4;
+    }
+
+    #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+    pub struct DeviceConfig: u8 {
+        const SPI_MODE_0_AND_3 = 0b0 << 4;
+        const SPI_MODE_1_AND_2 = 0b1 << 4;
+
+        const SOFT_RESET_CONFIG = 0b1;
+    }
+
+    #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+    pub struct PowerManagement: u8 {
+        const TEMP_DISABLE = 0b1 << 5;
+
+        const IDLE = 0b1 << 4;
+
+        const GYRO_OFF = 0b00 << 2;
+        const GYRO_STANDBY = 0b01 << 2;
+        const GYRO_LOW_NOISE = 0b11 << 2;
+
+        const ACCEL_OFF = 0b00;
+        const ACCEL_LOW_POWER = 0b10;
+        const ACCEL_LOW_NOISE = 0b11;
+    }
+
+    #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+    pub struct GyroConfig: u8 {
+        const GYRO_FS_2000 = 0b000 << 5;
+        const GYRO_FS_1000 = 0b001 << 5;
+        const GYRO_FS_500 = 0b010 << 5;
+        const GYRO_FS_250 = 0b011 << 5;
+        const GYRO_FS_125 = 0b100 << 5;
+        const GYRO_FS_62_5 = 0b101 << 5;
+        const GYRO_FS_31_25 = 0b110 << 5;
+        const GYRO_FS_15_625 = 0b111 << 5;
+
+        const GYRO_ODR_8kHz = 0b0011;
+        const GYRO_ODR_4kHz = 0b0100;
+        const GYRO_ODR_2kHz = 0b0101;
+        const GYRO_ODR_1kHz = 0b0110;
+        const GYRO_ODR_200Hz = 0b0111;
+        const GYRO_ODR_100Hz = 0b1000;
+        const GYRO_ODR_50Hz = 0b1001;
+        const GYRO_ODR_25Hz = 0b1010;
+        const GYRO_ODR_12_5Hz = 0b1011;
+        const GYRO_ODR_500Hz = 0b1111;
+    }
+
+    #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+    pub struct AccelConfig: u8 {
+        const ACCEL_FS_16 = 0b000 << 5;
+        const ACCEL_FS_8 = 0b001 << 5;
+        const ACCEL_FS_4 = 0b010 << 5;
+        const ACCEL_FS_2 = 0b011 << 5;
+
+        const ACCEL_ODR_8kHz = 0b0011;
+        const ACCEL_ODR_4kHz = 0b0100;
+        const ACCEL_ODR_2kHz = 0b0101;
+        const ACCEL_ODR_1kHz = 0b0110;
+        const ACCEL_ODR_200Hz = 0b0111;
+        const ACCEL_ODR_100Hz = 0b1000;
+        const ACCEL_ODR_50Hz = 0b1001;
+        const ACCEL_ODR_25Hz = 0b1010;
+        const ACCEL_ODR_12_5Hz = 0b1011;
+        const ACCEL_ODR_6_25Hz = 0b1100;
+        const ACCEL_ODR_3_125Hz = 0b1101;
+        const ACCEL_ODR_1_5625Hz = 0b1110;
+        const ACCEL_ODR_500Hz = 0b1111;
+    }
+}
+
+#[derive(Debug, Clone, Copy, Default, PartialEq, Eq)]
+pub enum Bank {
+	#[default]
+    Bank0,
+    Bank1,
+    Bank2,
+    Bank3,
+    Bank4,
+}
+
+impl Bank {
+    pub fn value(self) -> u8 {
+        match self {
+            Bank::Bank0 => 0,
+            Bank::Bank1 => 1,
+            Bank::Bank2 => 2,
+            Bank::Bank3 => 3,
+            Bank::Bank4 => 4,
+        }
+    }
+}
+
+impl BankSelect {
+    pub const ADDR: u8 = 0x76;
+    pub const BANK: Bank = Bank::Bank0;
+}
+
+impl DeviceConfig {
+    pub const ADDR: u8 = 0x11;
+    pub const BANK: Bank = Bank::Bank0;
+}
+
+impl PowerManagement {
+    pub const ADDR: u8 = 0x4E;
+    pub const BANK: Bank = Bank::Bank0;
+}
+
+impl GyroConfig {
+    pub const ADDR: u8 = 0x4F;
+	pub const BANK: Bank = Bank::Bank0;
+}
+
+impl AccelConfig {
+    pub const ADDR: u8 = 0x50;
+	pub const BANK: Bank = Bank::Bank0;
+}
