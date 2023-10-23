28c685b9bf33e4c4e0d6413f6fd335236a1689cd
Gavaletz, Tyler Andrew <tgavaletz@gatech.edu>
2023-10-22 21:53:05 -0400

    continue to work on led driver

--- examples/ledDriver.rs
+++ examples/ledDriver.rs
@@ -8,12 +8,46 @@ mod app {
     use teensy4_pins::common::*;
 
     use teensy4_bsp as bsp;
-    use bsp::board;
+    use bsp::board as board;
 
     use bsp::hal as hal;
     use hal::gpio::Output;
 
     use rtic_monotonics::systick::*;
 
-    
+    //local because this is currently the only place where this LED is used
+    #[local]
+    struct local {
+        //idk if pin 7 is the right pin or not? Ask Nate
+        led: rbg_LED_Driver<Output<P7>> 
+    }
+
+    #[shared]
+    struct shared {
+        //empty because LED currently isn't shared with anything
+    }
+
+    #[init]
+    fn init (ctx: init::Context) -> (Shared, Local) {
+        let board::Resources {
+            pins,
+            mut gpio2, //is this gpio correct and if it is/isn't how do I know?
+            ..
+        } = board::t41(ctx.device);
+
+        let systick_token = rtic_monotonics::create_systick_token!();
+        Systick::start(ctx.core.SYST, 36_000_000, systick_token);
+
+        //if pin 7 isn't the correct PIN then I need to make sure to change it here as well
+        let led = gpio2.output(pins.p7);
+
+        blink_led::spawn().ok();
+
+        (
+            Shared {},
+            Local {
+                led,
+            },
+        )
+    }
 }
