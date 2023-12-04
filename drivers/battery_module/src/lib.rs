9ee8df49d96bee11ca57fb727211b5f372803996
Tejas Mehta <tmthecoder@gmail.com>
2023-12-03 21:10:51 -0500

    change reading conversions

--- drivers/battery_module/src/lib.rs
+++ drivers/battery_module/src/lib.rs
@@ -10,10 +10,6 @@ const MIN_SAFE_VOLTAGE_READ: f32 = 1.923;
 
 const VOLTAGE_READ_RANGE: f32 = MAX_SAFE_VOLTAGE_READ - MIN_SAFE_VOLTAGE_READ;
 
-const ADC_CR1_AWDCH_POS: u8 = 0;
-
-const ADC_CR1_AWDCH_2: u32 = 0x04 << ADC_CR1_AWDCH_POS;
-
 const ADC_MAX: f32 = 4096.0;
 
 const ADC_RATIO: f32 = 3.3 / ADC_MAX;
@@ -29,23 +25,22 @@ impl<const T: u8> BatteryAdc<T> {
 }
 
 struct AdcChannel<P, const T: u8> {
-    pin: AnalogInput<P, T>
+    pin: AnalogInput<P, T>,
 }
 
 impl<P: Pin<T>, const T: u8> Channel<Adc<T>> for AdcChannel<P, T> {
     type ID = u32;
 
     fn channel() -> u32 {
-        ADC_CR1_AWDCH_2
+        P::INPUT
     }
 }
 
-impl<P: Pin<T>, const T: u8> OneShot<Adc<T>, f32, AdcChannel<P, T>> for BatteryAdc<T> {
+impl<P: Pin<T>, const T: u8> OneShot<Adc<T>, u16, AdcChannel<P, T>> for BatteryAdc<T> {
     type Error = BatteryError;
 
-    fn read(&mut self, channel: &mut AdcChannel<P, T>) -> nb::Result<f32, Self::Error> {
-        let reading = self.adc.read_blocking(&mut channel.pin);
-        Ok(reading as f32 * ADC_RATIO)
+    fn read(&mut self, channel: &mut AdcChannel<P, T>) -> nb::Result<u16, Self::Error> {
+        Ok(self.adc.read_blocking(&mut channel.pin))
     }
 }
 
@@ -55,7 +50,7 @@ pub enum BatteryError {
 
 pub struct Battery<P, const T: u8> {
     last_percentage: f32,
-    raw_reading: u8,
+    raw_reading: u16,
     adc: BatteryAdc<T>,
     channel: AdcChannel<P, T>,
 }
@@ -66,7 +61,7 @@ impl<P: Pin<T>, const T: u8> Battery<P, T> {
             last_percentage: 0.0,
             raw_reading: 0,
             adc,
-            channel: AdcChannel { pin }
+            channel: AdcChannel { pin },
         }
     }
 
@@ -75,13 +70,18 @@ impl<P: Pin<T>, const T: u8> Battery<P, T> {
             .adc
             .read(&mut self.channel)
             .map_err(|_e| BatteryError::ReadError)?;
-        self.last_percentage = (result - MIN_SAFE_VOLTAGE_READ) / VOLTAGE_READ_RANGE;
-        self.raw_reading = (self.last_percentage * 255.0) as u8;
+        // TODO map the `last_percentage value to the actual battery percentage`
+        // self.last_percentage = (result - MIN_SAFE_VOLTAGE_READ) / VOLTAGE_READ_RANGE;
+        self.raw_reading = result;
         Ok(())
     }
 
     pub fn get_percentage(&self) -> f32 {
         self.last_percentage
+    }
+
+    pub fn get_raw(&self) -> u16 {
+        self.raw_reading
     }
 
     pub fn is_critical(&self) -> bool {
