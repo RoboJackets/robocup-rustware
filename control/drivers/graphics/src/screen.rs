352f9482c14098225507ab006f75420fa82fe81f
jvogt23 <91633361+jvogt23@users.noreply.github.com>
2025-11-11 20:01:44 -0500

    Add docstrings

--- control/drivers/graphics/src/screen.rs
+++ control/drivers/graphics/src/screen.rs
@@ -10,6 +10,10 @@ use {
     crate::startup_screen::StartScreen,
 };
 
+/// States for the onboard display that determine what is shown on screen.
+/// Start state shows only the start screen by default.
+/// Error state shows a title and description of any error.
+/// MainLoop state shows a list of important information about the current status of the robot.
 #[derive(Copy, Clone, Eq, PartialEq, Debug)]
 pub enum DisplayState {
     Start,
@@ -17,6 +21,11 @@ pub enum DisplayState {
     MainLoop,
 }
 
+/// Trait for a Screen that extends from DrawTarget.
+/// initDisp must initialize the display and return a Result<(), Self::DisplayError>
+/// clearDisp must remove all contents from the display
+/// flushDisp must send to the display all data currently in the queue if it exists.
+///     Returns Result<(), Self::DisplayError>
 pub trait ScreenDisplay<Color>: DrawTarget<Color = Color> {
     type DisplayError;
     fn initDisp(&mut self) -> Result<(), Self::DisplayError>;
@@ -24,11 +33,18 @@ pub trait ScreenDisplay<Color>: DrawTarget<Color = Color> {
     fn flushDisp(&mut self) -> Result<(), Self::DisplayError>;
 }
 
+/// Container for errors in sending data to the display and errors in the display interface.
+/// DrawError::Draw maps errors in sending data, such as the draw() command for Drawables.
+/// DrawError::DisplayInterface maps errors in commands that act on a ScreenDisplay.
 pub enum DrawError<D1, D2> {
     DisplayInterface(D1),
     Draw(D2),
 }
 
+/// Struct representing the onboard display for a robot and a state machine for that display.
+/// Implemented where the display has trait ScreenDisplay.
+/// If the current state is MainLoop, displays current contents of data whenever draw() is called.
+/// If the current state is Error, displays errors whenever draw() is called.
 pub struct Screen<'a, D>
 where
     D: ScreenDisplay<BinaryColor>,
@@ -42,6 +58,9 @@ impl<'a, D> Screen<'a, D>
 where
     D: ScreenDisplay<BinaryColor> + DrawTarget<Color = BinaryColor>,
 {
+    /// Initialize a new Screen with defaults for data and errors.
+    /// Default state is DisplayState::Start.
+    /// Requires robot_id: u16, team: &'a str, display: D
     pub fn new(robot_id: u16, team: &'a str, display: D) -> Self {
         let instance = Screen {
             state: DisplayState::Start,
@@ -52,12 +71,16 @@ where
         return instance;
     }
 
+    /// Initializes and clears the display.
+    /// Returns Result<(), D::DisplayError>.
     pub fn init_display(&mut self) -> Result<(), D::DisplayError> {
         self.display.initDisp()?;
         self.display.clearDisp();
         Ok(())
     }
 
+    /// Draws to the screen and flushes the queue, depending on the current state.
+    /// Does not change the current data to be displayed; only sends the data to the screen.
     pub fn draw(&mut self) -> Result<(), DrawError<D::DisplayError, D::Error>> {
         self.display.clearDisp();
         match self.state {
@@ -81,6 +104,11 @@ where
         Ok(())
     }
 
+    /// Sets current state to DisplayState::MainLoop and sets the current data to the input params.
+    /// battery_percent: u32; the current battery percentage.
+    /// kicker_charged: bool; whether or not the kicker is currently ready.
+    /// ball_sense: bool; whether or not the robot has the ball.
+    /// latency: u32; the radio latency. Currently reserved for future use.
     pub fn main_loop_update(
         &mut self,
         battery_percent: u32,
@@ -95,6 +123,9 @@ where
         self.state = DisplayState::MainLoop;
     }
 
+    /// Sets the current state to DisplayState::Error and sets the current data to the input params.
+    /// heading: &'a str; the heading used to describe the error type.
+    /// message: alloc::string::String; detailed description of the error.
     pub fn error_update(&mut self, heading: &'a str, message: alloc::string::String) {
         self.errors.update(heading, message);
         self.state = DisplayState::Error;
