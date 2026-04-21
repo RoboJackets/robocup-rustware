a2ba73f667846437490ebc6e270e4546bf7fde51
N8BWert <n8.wert.b@gmail.com>
2026-04-21 19:29:34 -0400

    refactor get_team_and_id to be easier to read

--- control/src/selector.rs
+++ control/src/selector.rs
@@ -37,22 +37,11 @@ pub fn get_team_and_id<P0, P1, P2, P3>(
     p2: &Input<P2>,
     p3: &Input<P3>,
 ) -> (Team, u8) {
-    match (p0.is_set(), p1.is_set(), p2.is_set(), p3.is_set()) {
-        (false, false, false, false) => (Team::Yellow, 7),
-        (false, false, false, true) => (Team::Yellow, 6),
-        (false, false, true, false) => (Team::Yellow, 5),
-        (false, false, true, true) => (Team::Yellow, 4),
-        (false, true, false, false) => (Team::Yellow, 3),
-        (false, true, false, true) => (Team::Yellow, 2),
-        (false, true, true, false) => (Team::Yellow, 1),
-        (false, true, true, true) => (Team::Yellow, 0),
-        (true, false, false, false) => (Team::Blue, 7),
-        (true, false, false, true) => (Team::Blue, 6),
-        (true, false, true, false) => (Team::Blue, 5),
-        (true, false, true, true) => (Team::Blue, 4),
-        (true, true, false, false) => (Team::Blue, 3),
-        (true, true, false, true) => (Team::Blue, 2),
-        (true, true, true, false) => (Team::Blue, 1),
-        (true, true, true, true) => (Team::Blue, 0),
-    }
+    let team = if p0.is_set() {
+        Team::Blue
+    } else {
+        Team::Yellow
+    };
+    let id = (!p1.is_set() as u8) << 2 | (!p2.is_set() as u8) << 1 | (!p3.is_set() as u8);
+    (team, id)
 }
